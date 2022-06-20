198d1163d838764ddc6c453efcc82bf692a39e72
Anas Tazir <atazir2018@gmail.com>
2022-06-21 01:13:48 +0530

    add axios routes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
-import { FetchData } from '../types/fetch';
 
-const API = axios.create({ baseURL: 'https://jsonplaceholder.typicode.com' });
+const API = axios.create({ baseURL: 'http://127.0.0.1:5000' });
 
-export const fetch_users = () => API.get<Array<FetchData>>("/users");
+export const fetch_movies = (id:any) => API.get<any>(`/movies/fetch/${id}`);
+
+export const recommend_movies = (id:any) => API.get<any>(`/movies/recommend/${id}`);
+
