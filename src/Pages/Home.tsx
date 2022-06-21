b6e4f102c5e705508ff9aa0d3f1e1b47e6d29b4d
Anas Tazir <atazir2018@gmail.com>
2022-06-21 23:50:03 +0530

    add grid

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { trendingMovies} from '../actions/movie';
+import {ExpState} from "../reducers/index"
+import MovieCard from '../Components/MovieCard';
 
 export const Home = () => {
+  const dispatch = useDispatch();
+  const movieLists = useSelector((state: ExpState) => state.trendingReducer.movieLists);
+  const loading = useSelector((state: ExpState) => state.trendingReducer.loading);
+
+  useEffect(() => {
+    dispatch(trendingMovies());
+  },[])
   return (
-    <div>Hoasdfasdfsadfadsme</div>
+    <div className="grid md:grid-cols-3 lg:grid-cols-5 sm:gird-cols-1 gap-0">
+    {loading && <div>Loading...</div>}
+    {movieLists && movieLists.map((movie:any, index:number) => {
+            return (
+              <MovieCard key={index} movie={movie} />
+            )
+          },)}
+    </div>
   )
 }
