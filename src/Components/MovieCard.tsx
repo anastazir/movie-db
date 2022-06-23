2b06a6b174d605db1736e6d88be9507137917d24
Anas Tazir <atazir2018@gmail.com>
2022-06-23 13:00:37 +0530

    style more info button

--- src/Components/MovieCard.tsx
+++ src/Components/MovieCard.tsx
@@ -1,3 +1,4 @@
+import { InformationCircleIcon } from "@heroicons/react/solid";
 import { useNavigate } from "react-router-dom";
 
 const MovieCard = (props: any) => {
@@ -12,12 +13,14 @@ const MovieCard = (props: any) => {
 
     return (
         <div key={key} className="relative bg-black rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-            <img className="rounded-t-lg m-auto" src={ `${image500}${movie.poster_path}`} alt="" />
+            <img className="rounded-lg m-auto" src={ `${image500}${movie.poster_path}`} alt="" />
             <div className="p-5  ">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{movie.original_title} {movie.release_date}</h5>
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">{movie.original_title} {movie.release_date}</h5>
               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{movie.overview}</p>
             </div>
-              <button onClick={handleClick} className="text-center absolute  text-indigo-400 font-bold rounded bottom-0 left-0 focus:outline-none bg-gray-900 border-2 border-indigo-400">Details</button>
+            <button onClick={handleClick} className="h-5 w-5 md:h-8 md:w-8 absolute bottom-0 left-0 cursor-pointer">
+               <InformationCircleIcon />
+            </button>
         </div>
     )
 }
