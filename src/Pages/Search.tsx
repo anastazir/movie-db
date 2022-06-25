72c434e5e8c514f1d8ffce6e5c200b4c80b695f1
Anas Tazir <atazir2018@gmail.com>
2022-06-25 13:35:51 +0530

    style search page

--- src/Pages/Search.tsx
+++ src/Pages/Search.tsx
@@ -2,32 +2,29 @@ import { useEffect } from 'react'
 import {useParams} from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { searchMovies } from '../actions/movie';
+import RecCard from '../Components/RecCard';
 
 const Search = () => {
     const dispatch = useDispatch();
     const {search} = useParams();
-    const movies = useSelector((state: any) => state.movieReducer.moviesList);
-    useEffect(() => {
-        dispatch(searchMovies(search as string));
-            // console.log('useEffect', details);
-        },[])
+    const movies = useSelector((state: any) => state.searchReducer.moviesList);
+    const loading = useSelector((state: any) => state.searchReducer.loading);
+    
+    useEffect(() =>{
+      dispatch(searchMovies(search as string));
+
+    }, [search])
 
     return (
-        <>
-            <div>Search</div>
-            <div>{search}</div>
-            {movies && movies.map((item:any, index:number) => {
-            return (
-              <div key={index}>
-                <h1>{item.name}</h1>
-                <h2>{item.imdb_id}</h2>
-                <p>{item.popularity}</p>
-                <p>{item.year}</p>
-                <p>{item.poster}</p>
-              </div>
-            )
-          },)}
-        </>
+      <>
+        <div className="grid md:grid-cols-3 lg:grid-cols-5 sm:gird-cols-1 gap-0">
+          {loading && <div>Loading...</div>}
+          {!loading && movies.map((item:any, index:number) => {
+          return (
+            <RecCard movie={item} index={index} />
+          )},)}
+        </div>
+      </>
     )
 }
 
