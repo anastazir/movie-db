cf886d36047c9925ecbdbab7a6473f0a8cfa49fe
Anas Tazir <atazir2018@gmail.com>
2022-06-26 05:25:38 +0530

    add API for genres search

--- src/api/index.ts
+++ src/api/index.ts
@@ -14,3 +14,5 @@ export const trending_movies = (page:number) => API2.get<any>(`trending/movie/da
 export const movie_details = (id:any) => API2.get<any>(`movie/${id}?api_key=26ba5e77849587dbd7df199727859189`)
 
 export const get_crew = (imdb_id:any) => API3.get<any>(`?i=${imdb_id}&apikey=ac6564b1`)
+
+export const genre_search = (genres:string, page:number) => API2.get<any>(`discover/movie?api_key=26ba5e77849587dbd7df199727859189&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genres}`)
