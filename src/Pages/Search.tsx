5c60f74a1727cffe2c4b63c774fd7061bc43312c
Anas Tazir <atazir2018@gmail.com>
2022-06-25 17:45:53 +0530

    add padding to grid

--- src/Pages/Search.tsx
+++ src/Pages/Search.tsx
@@ -3,6 +3,7 @@ import {useParams} from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { searchMovies } from '../actions/movie';
 import RecCard from '../Components/RecCard';
+import Footer from '../Components/Footer';
 
 const Search = () => {
     const dispatch = useDispatch();
@@ -17,13 +18,14 @@ const Search = () => {
 
     return (
       <>
-        <div className="grid md:grid-cols-3 lg:grid-cols-5 sm:gird-cols-1 gap-0">
+        <div className="grid sm:gird-cols-2 md:grid-cols-3 lg:grid-cols-5  gap-6 overflow-hidden pb-9 px-10 py-5">
           {loading && <div>Loading...</div>}
           {!loading && movies.map((item:any, index:number) => {
           return (
             <RecCard movie={item} index={index} />
           )},)}
         </div>
+      <Footer />
       </>
     )
 }
