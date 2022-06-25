eb18fc166fac7a58c45a1ee0a2921e90acbb4760
Anas Tazir <atazir2018@gmail.com>
2022-06-25 17:49:41 +0530

    resize row component

--- src/Components/Row.tsx
+++ src/Components/Row.tsx
@@ -24,12 +24,11 @@ const Row = () => {
         return <div>Loading...</div>
     }
     return (
-      <div className="h-20 space-y-0.5 md:space-y-2">
-        <h2 className="text-center  cursor-pointer text-lg font-semibold text-[#e5e5e5] transition duration-200 hover:text-white">Recommended</h2>
-            <div className="group relative">
-                <ChevronLeftIcon  className="absolute top-0 bottom-0 m-auto h-9 w-9 left-2 opacity-0 cursor-pointer transition hover:scale-125 
-                group-hover:opacity-100" onClick={()=>handleClick("left")}/>
-                <div ref={rowRef} className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2">
+      <div className=" h-100 space-y-0.5 md:space-y-2">
+        <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">Recommended</h2>
+            <div className="group relative ">
+                <ChevronLeftIcon  className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100" onClick={()=>handleClick("left")}/>
+                <div ref={rowRef} className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2 -z-10">
                 {recLists  && recLists.map((movie:any, index:number) => {
                       return (
                         <Thumbnail key={index} movie={movie}/>
