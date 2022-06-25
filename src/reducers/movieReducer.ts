c5654549fd0d106c38935dfd1d5f51750a4e9a5a
Anas Tazir <atazir2018@gmail.com>
2022-06-26 05:21:58 +0530

    update movieReducer

--- src/reducers/movieReducer.ts
+++ src/reducers/movieReducer.ts
@@ -1,32 +1,12 @@
-import * as actionTypes from "../constants/exp"
-import { MovieDetails } from "../types/fetch"
 
-
-interface FetchedAction  {
-    type: "FETCHED"
-    data : MovieDetails
-}
-
-interface RecommendAction  {
-  type: "RECOMMENDATIONS"
-  data : any
-}
-
-interface FetchingAction {
-  type: "FETCHING"
-}
-
-const movieReducer = (state: any = {details: [], loading: false, recommendations: []}, action: FetchedAction | FetchingAction | RecommendAction) => {
+const movieReducer = (state: any = {details: [], loading: false, movieLists: []}, action: any) => {
     switch (action.type){
         case "FETCHED":
-          console.log({...state, details: action.data, loading: false});
           return {details: action.data, loading: false}
         case "FETCHING":
           return {...state, loading: false}
         case "RECOMMENDATIONS":
-          console.log(JSON.stringify(action.data));
-          
-          return {...state, recommendations: action.data}
+          return {...state, movieLists: action.data}
         default:
           return state
     }
