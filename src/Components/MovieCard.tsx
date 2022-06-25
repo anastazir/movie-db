be58734a5058c9f6b6a1eb30c314aab7c7050e82
Anas Tazir <atazir2018@gmail.com>
2022-06-25 17:50:08 +0530

    add transition to movie card

--- src/Components/MovieCard.tsx
+++ src/Components/MovieCard.tsx
@@ -1,26 +1,22 @@
-import { InformationCircleIcon } from "@heroicons/react/solid";
+// import { InformationCircleIcon } from "@heroicons/react/solid";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const MovieCard = (props: any) => {
+const MovieCard = ({movie, key}: any) => {
     const navigate = useNavigate();
-    const {movie, key} = props;
-    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
+    const [showDiv, setShowDiv] = useState(false)
+    const handleClick = () => {
         navigate(`/movie/details/${movie.id}`);
     }
 
     const image500 = "https://image.tmdb.org/t/p/w500"
 
     return (
-        <div key={key} className="relative bg-black rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div key={key} onClick={handleClick} className="relative group cursor-pointer transition duration-200 ease-out md:hover:scale-105">
             <img className="rounded-lg m-auto" src={ `${image500}${movie.poster_path}`} alt="" />
-            <div className="p-5  ">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">{movie.original_title} {movie.release_date}</h5>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{movie.overview}</p>
-            </div>
-            <button onClick={handleClick} className="h-5 w-5 md:h-8 md:w-8 absolute bottom-0 left-0 cursor-pointer">
-               <InformationCircleIcon />
-            </button>
+            {/* <button className="opacity-0 transition hover:scale-125 group-hover:opacity-100 h-5 w-5 md:h-8 md:w-8 absolute bottom-0 left-0 cursor-pointer">
+               <InformationCircleIcon / >
+            </button> */}
         </div>
     )
 }
