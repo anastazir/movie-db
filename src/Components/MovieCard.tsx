df1c4867d50f586cb79514a34b6c18bc7466ef97
Anas Tazir <atazir2018@gmail.com>
2022-06-26 05:25:07 +0530

    add links to divs

--- src/Components/MovieCard.tsx
+++ src/Components/MovieCard.tsx
@@ -1,10 +1,8 @@
 // import { InformationCircleIcon } from "@heroicons/react/solid";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const MovieCard = ({movie, key}: any) => {
     const navigate = useNavigate();
-    const [showDiv, setShowDiv] = useState(false)
     const handleClick = () => {
         navigate(`/movie/details/${movie.id}`);
     }
@@ -13,7 +11,12 @@ const MovieCard = ({movie, key}: any) => {
 
     return (
         <div key={key} onClick={handleClick} className="relative group cursor-pointer transition duration-200 ease-out md:hover:scale-105">
-            <img className="rounded-lg m-auto" src={ `${image500}${movie.poster_path}`} alt="" />
+            <Link to={`/movie/details/${movie.id}`}>
+                <img className="rounded-lg m-auto hover:opacity-80 transition-all duration-200" src={ `${image500}${movie.poster_path}`} alt="" />
+            </Link>
+            <h3 className="flex flex-wrap top-0 left-0 opacity-0 transition  group-hover:opacity-100 absolute cursor-pointer">
+                {movie.original_title}
+            </h3>
             {/* <button className="opacity-0 transition hover:scale-125 group-hover:opacity-100 h-5 w-5 md:h-8 md:w-8 absolute bottom-0 left-0 cursor-pointer">
                <InformationCircleIcon / >
             </button> */}
