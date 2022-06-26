4512166dae7beff554538030f7c727819d2137a6
Anas Tazir <atazir2018@gmail.com>
2022-06-26 17:53:33 +0530

    save movies to localStorage

--- src/Pages/Movie.tsx
+++ src/Pages/Movie.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import {useParams} from 'react-router-dom';
 import { getMovieData } from '../actions/movie';
@@ -6,18 +6,26 @@ import {ExpState} from "../reducers/index"
 import Row from '../Components/Row';
 import Details from '../Components/Details';
 import Footer from '../Components/Footer';
-const Movie = () => {
-    const details = useSelector((state: ExpState) => state.movieReducer.details);
-    const loading = useSelector((state: ExpState) => state.movieReducer.loading);
-    const dispatch = useDispatch();
-    const {id} = useParams();
-    const image500 = "https://image.tmdb.org/t/p/original"
+import { HeartIcon, EyeIcon, EyeOffIcon } from '@heroicons/react/outline';
+import { addToWatchlist, isInWatchlist, removeFromWatchlist } from '../localStorage/wishList';
 
+const Movie = () => {
+  const {id} = useParams();
+  const details = useSelector((state: ExpState) => state.movieReducer.details);
+  const loading = useSelector((state: ExpState) => state.movieReducer.loading);
+  const loadingRec = useSelector((state: ExpState) => state.movieReducer.loadingRec);
+  const dispatch = useDispatch();
+  const [seen, setSeen] = useState(false);
+    const image500 = "https://image.tmdb.org/t/p/original"
+    const [isFav, setIsFav] = useState(isInWatchlist(id as number | undefined))
+    console.log(isFav);
+    
+    const iconClasses = "h-9 w-9 absolute md:bottom-0 md:right-[50%] cursor-pointer transition-all duration-200 "
     useEffect(() => {
       window.scrollTo(0, 0)
       dispatch(getMovieData(id));
     },[id])
-    
+
     if(loading){
       return(
         <div className="flex justify-center items-center">
@@ -27,14 +35,21 @@ const Movie = () => {
         </div>
       )
     }
-
+    const handleHeart = () => {
+      if (!isFav) {
+        addToWatchlist(details)
+      }else{
+        removeFromWatchlist(details)
+      }
+      setIsFav(!isFav)
+    }
     return (
     <>
       <div className="flex flex-col space-y-2  md:space-y-4 lg:h-[65vh] lg:pb-12 ">
         <div className=" top-0 left-0 -z-10 w-screen ">
           <img src={`${image500}${details.backdrop_path}`} alt=""  className="object-cover "/>
         </div>
-        <div className=" absolute">
+        <div className=" md:absolute top-50 left-0">
           <h1 className="px-6 mt-7 text-2xl font-bold md:text-4xl lg:text-7xl pb-4 ">{details?.title || details?.name || details?.original_name}</h1>
           <p className="hidden md:block  px-6 max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">{details.overview}</p>
           <div className=" px-6 py-5">
@@ -48,10 +63,14 @@ const Movie = () => {
           <div className="px-6 ">
             <Details movie={details} />
           </div>
+          <div className={iconClasses}>
+            {seen ? <EyeIcon  /> : <EyeOffIcon />}
+            <HeartIcon onClick = {handleHeart}  className={(isFav ? " fill-red-500" : "")} />
+          </div>
         </div>
-        <section className="md:space-y-24">
+        {!loadingRec && (<section className="md:space-y-24">
           <Row />
-        </section>
+        </section>)}
         <Footer />
       </div>
     </>
