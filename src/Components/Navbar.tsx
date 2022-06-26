42b85fb952f51b400d161870650edfe3d309875f
Anas Tazir <atazir2018@gmail.com>
2022-06-27 00:41:52 +0530

    underline li based on page query

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,11 +1,13 @@
 import {SearchIcon} from "@heroicons/react/solid"
 import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-
+import { useNavigate, useLocation} from "react-router-dom"
+import useAuth from "../hooks/useAuth";
 const Navbar = () => {
   const [searchTerm, setSearchTerm] = useState("")
   const [isScrolled, setIsScrolled] = useState(false)
+  const {getUser, logout} = useAuth()
   const navigate = useNavigate()
+  const location = useLocation()
 
   const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(e.target.value)
@@ -25,27 +27,31 @@ const Navbar = () => {
     }
   },[])
 
+  const classes = "cursor-pointer text-sm font-light text-[#e5e5e5] transition duration-[.4s] hover:text-[#b3b3b3] lg:text-lg"
   const handleClick = () => {
-    console.log(searchTerm);
     navigate(`/movie/search/${searchTerm}`)
   }
+
   return (
     <header className={`${isScrolled && 'bg-black'}`}>
       <div className="flex items-center space-x-2 md:space-x-10">
-        <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" alt="" width={100} height={100}
+        <img onClick={() => navigate("/")} src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" alt="" width={100} height={100}
         className = "cursor-pointer object-contain"/>
         <ul className="hidden space-x-4 md:flex">
-          <li className = "cursor-pointer text-sm font-light text-[#e5e5e5] transition duration-[.4s] hover:text-[#b3b3b3] lg:text-lg">
+          <li onClick={() => navigate("/")} className = {classes + (location.pathname === "/" ? " underline" : "")}>
             Home
           </li>
-          <li className = "cursor-pointer text-sm font-light text-[#e5e5e5] transition duration-[.4s] hover:text-[#b3b3b3] lg:text-lg">
-            Movies
+          <li onClick={() => navigate("/movie/favorites")} className = {classes + (location.pathname === "/movie/favorites" ? " underline" : "")}>
+              Favorites
           </li>
-          <li className = "cursor-pointer text-sm font-light text-[#e5e5e5] transition duration-[.4s] hover:text-[#b3b3b3] lg:text-lg">
+          <li className = {classes}>
             Random
           </li>
-          <li className = "cursor-pointer text-sm font-light text-[#e5e5e5] transition duration-[.4s] hover:text-[#b3b3b3] lg:text-lg">
+          <li onClick={() => navigate("/movie/genres")} className = {classes + (location.pathname === "/movie/genres" ? " underline" : "")}>
             Genres
+          </li>
+          <li onClick={() => getUser() ? logout() : navigate("/login")} className = {classes + (location.pathname === "/login" ? " underline" : "")}>
+            {getUser() ? "Logout" : "Login" }
           </li>
         </ul>
       </div>
