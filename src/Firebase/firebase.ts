f22d68905cfde32fc5e4138ce307a59f8b714d4c
Anas Tazir <atazir2018@gmail.com>
2022-06-27 00:34:03 +0530

    add cofig file for firebase

---
+++ src/Firebase/firebase.ts
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {getAuth} from "firebase/auth"
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const auth = getAuth();
+
+
+export default app
+export {auth, db}
