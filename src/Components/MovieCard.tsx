6d7a8b91556ea80292f9b114f7c299256ddb4332
Anas Tazir <atazir2018@gmail.com>
2022-06-27 00:38:01 +0530

    add 'type' property to movieCard

--- src/Components/MovieCard.tsx
+++ src/Components/MovieCard.tsx
@@ -1,25 +1,26 @@
 // import { InformationCircleIcon } from "@heroicons/react/solid";
-import { Link, useNavigate } from "react-router-dom";
+import { XCircleIcon } from "@heroicons/react/solid";
+import { Link } from "react-router-dom";
+import { RootObject, RootObject2 } from "../interface/movieInterfaces";
+import { removeFromWatchlist } from "../localStorage/wishList";
 
-const MovieCard = ({movie, key}: any) => {
-    const navigate = useNavigate();
-    const handleClick = () => {
-        navigate(`/movie/details/${movie.id}`);
-    }
+type IFooBar = RootObject & RootObject2;
+interface Props{
+    movie: IFooBar
+    key: number
+    type: string
+}
+const MovieCard = ({movie, key, type}: Props) => {
 
     const image500 = "https://image.tmdb.org/t/p/w500"
-
     return (
-        <div key={key} onClick={handleClick} className="relative group cursor-pointer transition duration-200 ease-out md:hover:scale-105">
+        <div key={key} className="relative group cursor-pointer transition duration-200 ease-out md:hover:scale-105">
             <Link to={`/movie/details/${movie.id}`}>
                 <img className="rounded-lg m-auto hover:opacity-80 transition-all duration-200" src={ `${image500}${movie.poster_path}`} alt="" />
             </Link>
-            <h3 className="flex flex-wrap top-0 left-0 opacity-0 transition  group-hover:opacity-100 absolute cursor-pointer">
-                {movie.original_title}
-            </h3>
-            {/* <button className="opacity-0 transition hover:scale-125 group-hover:opacity-100 h-5 w-5 md:h-8 md:w-8 absolute bottom-0 left-0 cursor-pointer">
-               <InformationCircleIcon / >
-            </button> */}
+            {type === "favorites" && (<button onClick={()=>removeFromWatchlist(movie.id)} className="z-5 opacity-0 transition hover:scale-125 group-hover:opacity-100 h-5 w-5 md:h-8 md:w-8 absolute bottom-0 left-0 cursor-pointer">
+               <XCircleIcon / >
+            </button>)}
         </div>
     )
 }
