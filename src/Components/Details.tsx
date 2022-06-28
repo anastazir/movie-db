61ad96b9aa17d3f77e323dd694c3efb24c04ca8d
Anas Tazir <atazir2018@gmail.com>
2022-06-28 23:39:51 +0530

    update details component

--- src/Components/Details.tsx
+++ src/Components/Details.tsx
@@ -1,14 +1,11 @@
 const Details = ({movie}: any) => {
   return (
-    <div className=" md:max-w-lg md:text-lg lg:max-w-2xl">
+    <div className=" text-sm md:text-lg max-w-4xl">
       <div>Actors : {movie.Actors}</div>
-      <div>Released Date : {movie.Released}</div>
       <div>Director : {movie.Director}</div>
       <div>Rated : {movie.Rated}</div>
       <div>Vote : {movie.vote_average}</div>
-      <div>Runtime : {movie.runtime}</div>
       <div>Imdb ID : {movie.imdbID}</div>
-      <div>Adult : {movie.adult ? 'true' : 'flase'}</div>
     </div>
   )
 }
