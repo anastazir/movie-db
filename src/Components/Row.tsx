0074af8e0bc7f8b9c90a92159ce1c4aa21b6e674
Anas Tazir <atazir2018@gmail.com>
2022-06-30 16:49:38 +0530

    revamp Row componenet

--- src/Components/Row.tsx
+++ src/Components/Row.tsx
@@ -1,17 +1,12 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline';
 import { useRef } from 'react';
-import { useSelector } from 'react-redux';
-import { ExpState } from '../reducers';
 import Thumbnail from './Thumbnail';
 
-const Row = () => {
+const Row = ({title, recLists, loading}: any) => {
     const rowRef = useRef<HTMLDivElement>(null)
-    const recLists = useSelector((state: ExpState) => state.movieReducer.movieLists);
-    const loading = useSelector((state: ExpState) => state.movieReducer.loading);
     
     const handleClick = (direction: string) =>{
-        console.log("handleClick", rowRef.current);
-        
+
         if (rowRef.current){
             const {scrollLeft, clientWidth} = rowRef.current
             const scrollTo = direction === 'left' ? scrollLeft - clientWidth : scrollLeft + clientWidth
@@ -19,13 +14,14 @@ const Row = () => {
             rowRef.current.scrollTo({left: scrollTo, behavior: "smooth"})
         }
     }
-    
+
     if(loading) {
         return <div>Loading...</div>
     }
     return (
+    <section className="md:space-y-24">
       <div className=" h-100 space-y-0.5 md:space-y-2">
-        <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">Recommended</h2>
+        <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">{title}</h2>
             <div className="group relative ">
                 <ChevronLeftIcon  className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100" onClick={()=>handleClick("left")}/>
                 <div ref={rowRef} className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2 -z-10">
@@ -39,6 +35,7 @@ const Row = () => {
                 group-hover:opacity-100" onClick={()=>handleClick("right")} />
             </div>
       </div>
+    </section>
     )
 }
 
