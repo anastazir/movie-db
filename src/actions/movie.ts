a7885763a91f17c3f80b5ee86bbdfa417af5692e
Anas Tazir <atazir2018@gmail.com>
2022-06-30 16:44:55 +0530

    add and rename axios apis

--- src/actions/movie.ts
+++ src/actions/movie.ts
@@ -1,7 +1,6 @@
 import * as api from "../api/index"
 
-export const getMovieData  = (id: any) => async (dispatch: any) => {
-    console.log("getMovieData", id);
+export const getMovieData  = (id: string | undefined) => async (dispatch: any) => {
     try{
 
         dispatch({type: "FETCHING_DETAILS"})
@@ -9,21 +8,23 @@ export const getMovieData  = (id: any) => async (dispatch: any) => {
         const crew = await api.get_crew(data.imdb_id);
         dispatch({type: "FETCHED_DETAILS", data : {...data, ...crew.data}})
 
-        dispatch({type: "FETCHING_RECOMMENDATIONS"})
-        const recData = await api.recommend_movies(data.imdb_id);
-        dispatch({type: "FETCHED_RECOMMENDATIONS", data : recData.data})
     }
     catch(err) {
         console.log(err);
     }
 }
 
-export const getMovieRecommendation  = (id: any) => async (dispatch: any) => {
+export const getMovieRecommendation  = (id: string, genres: string) => async (dispatch: any) => {
+    dispatch({type: "FETCHING_RECOMMENDATIONS"})
     try{
+        // console.log(id, genres);
+        const promise = await Promise.all([api.get_similar_movies(id),  api.genre_search(genres), api.recommend_movies_web(id)])
+        // console.log("Promise.all", );
+        // const {data: similarMovies} = await api.get_similar_movies(id)
+        // const {data: genreRecommendations} = await api.genre_search(genres)
+        // const {data: webRecommendations} = await api.recommend_movies_web(id);
 
-        const {data} = await api.recommend_movies(id);
-
-        dispatch({type: "RECOMMENDATIONS", data : data})
+        dispatch({type: "FETCHED_RECOMMENDATIONS", data : promise.map((a : any) => a.data.results)})
     }
     catch(err) {
         console.log(err);
@@ -37,10 +38,10 @@ export const searchMovies = (search: string) => async (dispatch: any) => {
 
         const {data} = await api.search_movies(search);
 
-        dispatch({type: "SEARCHED", data : data})
+        dispatch({type: "SEARCHED", data : data.results})
     }
     catch(err) {
-        console.log(err);
+        console.log("searchMovies",err);
     }
 }
 
--- src/api/index.ts
+++ src/api/index.ts
@@ -5,9 +5,7 @@ const API2 = axios.create({ baseURL: 'https://api.themoviedb.org/3/'})
 const API3 = axios.create({ baseURL: 'http://www.omdbapi.com/'})
 // export const fetch_movies = (id:any) => API.get<any>(`/movies/fetch/${id}`);
 
-export const recommend_movies = (id:any) => API.get<any>(`/movies/recommend/${id}`);
-
-export const search_movies = (search:any) => API.get<any>(`/movies/search/${search}`);
+export const search_movies = (search:any, page:number = 1) => API2.get<any>(`search/movie?api_key=26ba5e77849587dbd7df199727859189&query=${search}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`);
 
 export const trending_movies = (page:number) => API2.get<any>(`trending/movie/day?api_key=26ba5e77849587dbd7df199727859189&language&page=${page}`)
 
@@ -15,4 +13,10 @@ export const movie_details = (id:any) => API2.get<any>(`movie/${id}?api_key=26ba
 
 export const get_crew = (imdb_id:any) => API3.get<any>(`?i=${imdb_id}&apikey=ac6564b1`)
 
-export const genre_search = (genres:string, page:number) => API2.get<any>(`discover/movie?api_key=26ba5e77849587dbd7df199727859189&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genres}`)
+export const genre_search = (genres:string, page:number = 1) => API2.get<any>(`discover/movie?api_key=26ba5e77849587dbd7df199727859189&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genres}`)
+
+export const get_similar_movies = (id:string) => API2.get<any>(`https://api.themoviedb.org/3/movie/${id}/similar?api_key=26ba5e77849587dbd7df199727859189&language=en-US&page=1`)
+
+export const recommend_movies_custom = (id:any) => API.get<any>(`/movies/recommend/${id}`);
+
+export const recommend_movies_web = (id:string) => API.get<any>(`https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=26ba5e77849587dbd7df199727859189&language=en-US&page=1`)
