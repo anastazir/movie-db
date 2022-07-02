04884fdb13c09f80acd7f25b2fa8d500bc69cdbf
Anas Tazir <atazir2018@gmail.com>
2022-07-02 22:47:27 +0530

    change image src

--- src/Components/Thumbnail.tsx
+++ src/Components/Thumbnail.tsx
@@ -2,15 +2,16 @@ import { useNavigate } from "react-router-dom";
 
 const Thumbnail = ({key, movie}: any) => {
   const navigate = useNavigate()
+  const image500 = "https://image.tmdb.org/t/p/w500"
 
   const handleClick = (e: any) => {
     e.preventDefault();
 
-    navigate(`/movie/details/${movie.tmdb_id}`);
+    navigate(`/movie/details/${movie.id}`);
   }
   return (
     <div className="relative min-w-[180px] cursor-pointer transition duration-200 ease-out  md:min-w-[260px] md:hover:scale-105">
-      <img key ={key} src={ movie.poster.replace("original", "w300")} alt="" className=" md:rounded cursor-pointer" onClick={handleClick}/>
+      <img key ={key} src={ `${image500}${movie.poster_path}`}  alt="" className=" md:rounded cursor-pointer" onClick={handleClick}/>
     </div>
   )
 }
