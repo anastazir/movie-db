53dd5f083f559c9fb4a669cec39168011b366248
Anas Tazir <atazir2018@gmail.com>
2022-07-02 22:56:08 +0530

    remove react helmet

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -34,7 +34,7 @@ const Navbar = () => {
 
   return (
     <>
-    <SEO />
+    {/* <SEO /> */}
     <header className={`${isScrolled && 'bg-black'}`}>
       <div className="flex items-center space-x-2 md:space-x-10">
         <img onClick={() => navigate("/")} src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" alt="" width={100} height={100}
