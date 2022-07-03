ac1f27fb03dcc80cc653abc03ae59d26f7e8df16
Anas Tazir <atazir2018@gmail.com>
2022-07-04 00:45:07 +0530

    get cast and crew info form redux

--- src/Components/Details.tsx
+++ src/Components/Details.tsx
@@ -1,11 +1,28 @@
-const Details = ({movie}: any) => {
+import { useSelector } from 'react-redux';
+import { ExpState } from '../reducers';
+
+const Details = () => {
+  const director = useSelector((state: ExpState) => state.movieReducer.director)
+  const cast_obj = useSelector((state: ExpState) => state.movieReducer.cast)
+  const cast = cast_obj?.map((c:any)=>{
+    return c.name
+  }).join(", ")
+  console.log(director);
+
   return (
     <div className=" text-sm md:text-lg max-w-4xl">
-      <div>Actors : {movie.Actors}</div>
-      <div>Director : {movie.Director}</div>
-      <div>Rated : {movie.Rated}</div>
-      <div>Vote : {movie.vote_average}</div>
-      <div>Imdb ID : {movie.imdbID}</div>
+        <div>
+          <span className="font-semibold">
+            Actors : 
+          </span>
+         {cast}
+        </div>
+        <div>
+          <span className="font-semibold">
+            Director : 
+          </span>
+        {director && director[0].name}
+        </div>
     </div>
   )
 }
